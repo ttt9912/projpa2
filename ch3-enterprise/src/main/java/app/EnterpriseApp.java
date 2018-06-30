e44b2f818e33816f91618e1f72bdb005a3f8966d
thts <tschachtli.th@gmail.com>
2018-06-30 13:47:44 +0200

    ch5

---
+++ ch3-enterprise/src/main/java/app/EnterpriseApp.java
@@ -0,0 +1,18 @@
+package app;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EnterpriseApp {
+
+    private static final String PU_NAME = "persistenceUnit";
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(PU_NAME);
+        EntityManager em = emf.createEntityManager();
+
+
+    }
+
+}
--- ch4-orm-relationships/src/main/java/entity/Badge.java
+++ ch4-orm-relationships/src/main/java/entity/Badge.java
@@ -1,8 +1,5 @@
 package entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
